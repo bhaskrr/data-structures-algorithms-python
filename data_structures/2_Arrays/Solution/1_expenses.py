12ee940455c801558a768791200da591a25ce072
Bhaskar Bordoloi <74664799+bhaskrr@users.noreply.github.com>
2024-08-29 21:27:19 +0530

    Update 1_expenses.py
    used functions for each sub question

--- data_structures/2_Arrays/Solution/1_expenses.py
+++ data_structures/2_Arrays/Solution/1_expenses.py
@@ -15,23 +15,34 @@
 # got a refund of 200$. Make a correction to your monthly expense list
 # based on this
 
-exp = [2200,2350,2600,2130,2190]
+expenses = [2200, 2350, 2600, 2130, 2190]
+months = ['January', 'February', 'March', 'April', 'May']
 
-# 1. In Feb, how many dollars you spent extra compare to January?
-print("In feb this much extra was spent compared to jan:",exp[1]-exp[0]) # 150
+def calculate_extra_expense_in_feb(expense_arr, months_arr):
+    extra_expense_in_feb = expenses[months.index('February')] - expenses[months.index('January')]
+    return extra_expense_in_feb
 
-# 2. Find out your total expense in first quarter (first three months) of the year
-print("Expense for first quarter:",exp[0]+exp[1]+exp[2]) # 7150
+def calculate_first_quarter_enpense(expense_arr):
+    first_quarter_enpense = 0
+    for i in range(3):
+        first_quarter_enpense += expense_arr[i]
+    return first_quarter_enpense
+    
+def check_exacly_2000_spent(expense_arr):
+    for i in range(len(expense_arr)):
+        if expense_arr[i] == 2000:
+            return True
+    return False
+    
+def add_month_and_expense(expense_arr, months_arr, month, amount):
+    expense_arr.append(amount)
+    months_arr.append(month)
+    return expense_arr, months_arr
+    
+expenses, months = add_month_and_expense(expenses, months, 'June', '1980')
 
-# 3. Find out if you spent exactly 2000 dollars in any month
-print("Did I spent 2000$ in any month? ", 2000 in exp) # False
+def returned_item(expense_arr, months_arr, month, amount):
+    expense_arr[months_arr.index(month)] -= amount
+    return expense_arr
 
-# 4. June month just finished and your expense is 1980 dollar. Add this item to our monthly expense list
-exp.append(1980)
-print("Expenses at the end of June:",exp) # [2200, 2350, 2600, 2130, 2190, 1980]
-
-# 5. You returned an item that you bought in a month of April and
-# got a refund of 200$. Make a correction to your monthly expense list
-# based on this
-exp[3] = exp[3] - 200
-print("Expenses after 200$ return in April:",exp) # [2200, 2350, 2600, 1930, 2190, 1980]
+expenses = returned_item(expenses, months, 'April', 200)
